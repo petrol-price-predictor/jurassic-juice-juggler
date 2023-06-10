2ed068a109c36cb2efa4a7a025ce59ccc0b8b5a8
Joshua Beilenhoff <jdbeil@Joshuas-MacBook-Air.local>
2023-06-10 12:08:38 +0200

    moved style functions and plot parameters into the src.visualization module for use in other notebooks

--- src/visualization.py
+++ src/visualization.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 from sklearn.metrics import accuracy_score, fbeta_score, recall_score, precision_score
 
@@ -20,3 +22,35 @@ def nice_summary(df):
             .reset_index() \
             .rename(columns={'index': 'Columns'}) \
             .replace({'Missing': 0, 'Missing %': 0}, '-')
+
+def set_rc_params(front_color='white', bg_color='#232742'):
+    # Set text color and axes color
+    plt.rcParams['text.color'] = front_color
+    plt.rcParams['axes.labelcolor'] = front_color
+    plt.rcParams['xtick.color'] = front_color
+    plt.rcParams['ytick.color'] = front_color
+    plt.rcParams['axes.edgecolor'] = front_color
+    plt.rcParams['grid.color'] = front_color
+
+    # Set the background color
+    plt.rcParams['axes.facecolor'] = bg_color
+
+    # Set the font
+    plt.rcParams['font.sans-serif'] = 'Arial'
+
+    # Set the grid alpha of the charts grid
+    plt.rcParams['axes.grid'] = True
+    plt.rcParams['grid.alpha'] = 0.1
+    
+
+def set_plot_options(figure, axis):
+
+    axis.margins(x=0)
+    plt.tight_layout()
+
+    #set legend text color
+    legend = axis.get_legend()
+    for text in legend.get_texts():
+        text.set_color('white')
+    figure.patch.set_alpha(0.0)
+
